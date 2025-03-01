e8aa94cd792702ed339a3609b8eb45a5f575251f
mnboy19 <jchrisboy0@gmail.com>
2025-03-01 00:58:44 -0600

    second commit

--- index.php
+++ index.php
@@ -7,21 +7,22 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Calculator</title>
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
-
-		<form action="includes/calc.inc.php" method="post">
-			<p>My own calculator!</p>
-			<input type="number" name="num1" placeholder="First number">
-			<select name="oper">
-				<option value="add">Addition		</option>
-				<option value="sub">Subtraction		</option>
-				<option value="div">Division		</option>
-				<option value="mul">Multiplication	</option>
-			</select>
-			<input type="number" name="num2" placeholder="Second number">
-			<button type="submit" name="submit">Calculate</button>
-		</form>
-
+		<div>
+			<form action="includes/calc.inc.php" method="post" class="calculator">
+				<p class="tittle">My own calculator!</p>
+				<input type="number" name="num1" placeholder="First number">
+				<select name="oper">
+					<option value="add">Addition		</option>
+					<option value="sub">Subtraction		</option>
+					<option value="div">Division		</option>
+					<option value="mul">Multiplication	</option>
+				</select>
+				<input type="number" name="num2" placeholder="Second number">
+				<button type="submit" name="submit">Calculate</button>
+			</form>
+		</div>
 	</body>
 </html>
